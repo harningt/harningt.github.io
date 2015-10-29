1ed52d1036fa961d2a4266036b44be52d2584c2f
Thomas Harning Jr <harningt@gmail.com>
2015-10-29 19:30:52 -0400

    makes some JS async

--- default.js
+++ default.js
@@ -1,4 +1,13 @@
+function loadStylesheet(src) {
+    if (document.createStyleSheet) {
+        document.createStyleSheet(src);
+    } else {
+        $("head").append($("<link rel='stylesheet' href='"+src+"' type='text/css' media='screen' />"));
+    }
+}
+
 $(function() {
+    loadStylesheet("/css/main.css");
     if ($.gaTracker) {
         $.gaTracker("UA-975643-2"); /* Google Analytics */
     }
